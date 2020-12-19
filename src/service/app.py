b82762ed564552a0f8c6f7bc8b0c6e7e41e88755
Lauri Vuorela <lauri.vuorela@gmail.com>
2020-12-19 16:47:22 +0100

    make endpoint more RESTful

--- src/service/app.py
+++ src/service/app.py
@@ -7,6 +7,6 @@ from service.base import BaseService
 class Nangijala(BaseService):
     name = "nangijala.lauri.af"
 
-    @http(("GET", "POST"), r"/renew/(?P<switch_uuid>[^/]+?)/?")
+    @http(("GET", "POST"), r"/switch/(?P<switch_uuid>[^/]+?)/renew/?")
     async def cards(self, request: web.Request, switch_uuid: str) -> web.Response:
         return web.json_response({"status": "ok"}, status=200)
